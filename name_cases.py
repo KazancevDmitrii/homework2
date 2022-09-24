3f911208b752d2513c0cbb68e2786e7342d25050
DEXP <kazancevdiiitrij@gmail.com>
2022-09-24 18:07:37 +0300

    404

---
+++ name_cases.py
@@ -0,0 +1,22 @@
+name = "Dmitrii"
+
+#2.3
+print("2.3")
+print ("Hello,", name+\
+       ", it's your homework")
+
+#2.4
+name = "Dmitrii /"
+print("2.4")
+print(name.lower(),"все буквы в нижнем регистре")
+print(name.upper(),"все буквы в верхнем регистре")
+print(name.title(),"первая буква заглавная")
+
+#2.5
+print("2.5")
+print("GLaDOS once said, 'The cake is a lie.'")
+
+#2.6
+print("2.6")
+name = "GLaDOS"
+print(name, "once said, 'The cake is a lie.'")
